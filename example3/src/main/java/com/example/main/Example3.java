0e633c4c49a38147022731330da15e0234528091
Amr Ibrahim <amrkhaledsamir.ibrahim@sicpa.com>
2025-09-09 18:58:48 +0800

    Example 3: added custom bean naming ways

---
+++ example3/src/main/java/com/example/main/Example3.java
@@ -0,0 +1,24 @@
+package com.example.main;
+
+import com.example.beans.Vehicle;
+import com.example.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Example3 {
+
+    public static void main(String[] args) {
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Vehicle veh1 = context.getBean("protonSaga",Vehicle.class);
+        System.out.println("Vehicle name from Spring Context: " + veh1.getName());
+
+        Vehicle veh2 = context.getBean("audi", Vehicle.class);
+        System.out.println("Vehicle name from Spring context: " + veh2.getName());
+
+        Vehicle veh3 = context.getBean("honda", Vehicle.class);
+        System.out.println("Vehicle name from Spring context: " + veh3.getName());
+
+
+    }
+}
