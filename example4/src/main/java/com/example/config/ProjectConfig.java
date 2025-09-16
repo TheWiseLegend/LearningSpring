ec55bfd241c713181c6df25715806709e2b323d9
Amr Ibrahim <amrkhaledsamir.ibrahim@sicpa.com>
2025-09-16 16:46:56 +0800

    Primary annotation

---
+++ example4/src/main/java/com/example/config/ProjectConfig.java
@@ -0,0 +1,36 @@
+package com.example.config;
+
+import com.example.beans.Vehicle;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class  ProjectConfig {
+
+    @Primary
+    @Bean(name="protonSaga")
+    Vehicle vehicle1(){
+        var veh = new Vehicle();
+        veh.setName("Proton Saga");
+        return veh;
+    }
+
+    @Bean(value = "audi")
+    Vehicle vehicle2(){
+        var veh = new Vehicle();
+        veh.setName("Audi");
+        return veh;
+    }
+
+    @Bean("honda")
+    Vehicle vehicle3(){
+        var veh = new Vehicle();
+            veh.setName("Honda");
+        return veh;
+    }
+
+
+
+
+}
