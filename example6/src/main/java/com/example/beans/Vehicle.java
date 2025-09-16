dd619527e1c166f3a2f4632ebc6d8e280085786b
Amr Ibrahim <amrkhaledsamir.ibrahim@sicpa.com>
2025-09-16 18:34:01 +0800

    Componet method and PostConstruct Annotations

---
+++ example6/src/main/java/com/example/beans/Vehicle.java
@@ -0,0 +1,27 @@
+package com.example.beans;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Vehicle {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @PostConstruct
+    public void intialize(){
+        this.name="Proton";
+    }
+
+    public void printHello(){
+        System.out.println("Hello World im coming from a bean");
+    }
+}
