42b503ca970153f727efcf25c69dd30d049fee83
Amr Ibrahim <amribrahim.dev@gmail.com>
2025-10-06 09:34:59 +0800

    loggers AOP

--- example_17/src/main/java/com/example/aspects/LoggerAspect.java
+++ example_17/src/main/java/com/example/aspects/LoggerAspect.java
@@ -1,5 +1,6 @@
 package com.example.aspects;
 
+import com.example.interfaces.LogAspect;
 import com.example.services.VehicleServices;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -23,6 +24,28 @@ public class LoggerAspect {
     @Around("execution(* com.example.services.*.*(..))")
     public void log(ProceedingJoinPoint joinPoint) throws Throwable {
         logger.info(joinPoint.getSignature().toString() + " method execution start");
+        Instant start = Instant.now();
+        joinPoint.proceed();
+        Instant finish = Instant.now();
+        long timeElapsed = Duration.between(start, finish).toMillis();
+        logger.info("Time took to execute the method : "+timeElapsed);
+        logger.info(joinPoint.getSignature().toString() + " method execution end");
+    }
+
+    @AfterThrowing(value = "execution(* com.example.services.*.*(..))", throwing = "ex")
+    public void logException(JoinPoint joinPoint, Exception ex) {
+        logger.log(Level.SEVERE, joinPoint.getSignature().toString() + " An exception was thrown with the help of @AfterThrowing which happened due to " + ex.getMessage());
+    }
+
+    @AfterReturning(value = "execution(* com.example.services.*.*(..))", returning = "returnValue")
+    public void logReturnValue(JoinPoint joinPoint, Object returnValue) {
+        logger.log(Level.INFO, joinPoint.getSignature().toString() + "Method was successfully processed with the status "
+            + returnValue.toString());
+    }
+
+    @Around("@annotation(com.example.interfaces.LogAspect)")
+    public void logWithAnnotation(ProceedingJoinPoint joinPoint) throws Throwable {
+        logger.info(joinPoint.toString() + " method execution start");
         Instant start = Instant.now();
         joinPoint.proceed();
         Instant finish = Instant.now();
