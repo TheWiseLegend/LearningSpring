011125b60d956f8f6983785e1296071f8bce37d2
Amr Ibrahim <amribrahim.dev@gmail.com>
2025-10-16 18:28:50 +0800

    test

--- example18/src/main/java/com/example/example18/controller/HomeController.java
+++ example18/src/main/java/com/example/example18/controller/HomeController.java
@@ -1,4 +1,13 @@
 package com.example.example18.controller;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Component
 public class HomeController {
+
+  @RequestMapping("/home")
+  public String displayHomePage(){
+    return "home.html";
+  }
 }
