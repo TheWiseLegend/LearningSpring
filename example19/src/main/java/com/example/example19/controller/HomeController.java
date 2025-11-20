e6c9a505aa7d3c674fbeebda5dad4b40c8c43e9d
Amr Ibrahim <amribrahim.dev@gmail.com>
2025-11-20 14:15:35 +0800

    test

--- example19/src/main/java/com/example/example19/controller/HomeController.java
+++ example19/src/main/java/com/example/example19/controller/HomeController.java
@@ -21,6 +21,7 @@ public class HomeController {
         "message", "Welcome to Spring School!",
         "description", "Learning Spring Boot with React",
         "bio", "This is Amr's Spring school"
+
     );
   }
 }
